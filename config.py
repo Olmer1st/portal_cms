0167866b3dcbbb7702865d523422ad6d61cb7a60
Olmer1st <olmermail@gmail.com>
2016-05-13 18:06:18 +0300

    initial commit

---
+++ config.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+DB = {
+    "host": "127.0.0.1",
+    "servername": "localhost",
+    "dbname": "simple_library",
+    "username": "library_admin",
+    "password": "123456",
+    "main_table": "book_data",
+    "charset": "utf8"
+}
+
+LIBRARY = {
+    "archives_path": "/media/olmer/Documents - HDD4/_Lib.rus.ec - Официальная/lib.rus.ec",
+    "inpx_file": "/media/olmer/Documents - HDD4/_Lib.rus.ec - Официальная/librusec_local_fb2.inpx",
+    "library_files":"/media/olmer/Documents - HDD4/library_files"
+}
---
+++ import_tools/extract_data.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import config as cfg
+import zipfile
+import MySQLdb
+
+
+
