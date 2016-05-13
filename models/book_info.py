fd75be9a4f45a2e06125e2ec22b9646966a487ff
Olmer1st <olmermail@gmail.com>
2016-05-14 00:00:52 +0300

    started to do things

--- models/book_info.py
+++ models/book_info.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+import config as cfg
+
 
 """AUTHOR;GENRE;TITLE;SERIES;SERNO;FILE;SIZE;LIBID;DEL;EXT;DATE;LANG;KEYWORDS;<CR><LF>"""
 
@@ -9,5 +11,18 @@ class BookInfo(object):
 
     mysql_connection = None
 
-    def __init__(self, line=None):
+    def __init__(self):
+        self._author = None
+        self._genre = None
+        self._title = None
+        self._series = None
+        self._serno = None
+        self._file = None
+        self._size = None
+        self.libid= None
+        self._del= None
+        self._ext= None
+        self._date= None
+        self._lang= None
+        self._keywords= None
         pass
